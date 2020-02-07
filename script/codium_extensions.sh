9a3c22823c892cc846c95f9ef397ce728f419893
Brendon Smith <br3ndonland@protonmail.com>
2020-02-07 11:48:41 -0500

    Automate VSCode/VSCodium extension installation
    This was the last step in removing the need for the Settings Sync
    extension (Shan.code-settings-sync), after moving my settings and
    keybindings into my dotfiles repo, and symlinking them into the
    appropriate directories.
    
    The shell script was quite easy to write. I based it on npm_globals.sh,
    and used the nice VSCode extension CLI.
    CLI: https://code.visualstudio.com/docs/editor/extension-gallery

---
+++ script/codium_extensions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+### ------------------ Install VSCode/VSCodium extensions ------------------ ###
+# Designed for use with Strap: https://github.com/MikeMcQuaid/strap
+# Run by strap-after-setup
+# CLI: https://code.visualstudio.com/docs/editor/extension-gallery
+install_codium_extensions() {
+  while read -r p; do
+    EXTENSIONS=$($EDITOR --list-extensions)
+    INSTALLED=$(echo "$EXTENSIONS" | grep -ce "^$p\$")
+    if [ "$INSTALLED" == "0" ]; then
+      echo "Installing $p."
+      $EDITOR --install-extension "$p"
+    else
+      echo "$p already installed."
+    fi
+  done <~/.dotfiles/.codium/codium-extensions.txt
+}
+# Editor command must be specified
+if [ -z "$1" ]; then
+  echo "No editor name was given for the codium_extensions.sh script. "
+  echo "Please try again, specifying editor (code, code-insiders, or codium)."
+  exit 1
+fi
+for i in "$@"; do
+  EDITOR=$i
+  echo "-> Installing extensions for $EDITOR"
+  install_codium_extensions
+done

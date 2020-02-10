c4157232b7acda30794de3fad5f884cfd8827ec0
Brendon Smith <br3ndonland@protonmail.com>
2020-02-10 14:53:04 -0500

    Add Karabiner Elements settings
    https://pqrs.org/osx/karabiner/
    https://github.com/pqrs-org/Karabiner-Elements
    https://pqrs.org/osx/karabiner/document.html#configuration-file-path

--- script/symlink.sh
+++ script/symlink.sh
@@ -33,6 +33,10 @@ symlink_dotfiles() {
         ~/Library/Application\ Support/VSCodium/User/snippets/vue.json
       ln -s -f ~/.dotfiles/.codium/vue.json \
         ~/Library/Application\ Support/Code\ -\ Insiders/User/snippets/vue.json
+      # https://pqrs.org/osx/karabiner/document.html#configuration-file-path
+      ln -s -f ~/.dotfiles/karabiner ~/.config
+      launchctl kickstart -k \
+        gui/"$(id -u)"/org.pqrs.karabiner.karabiner_console_user_server
     )
   else
     echo "-> Error: Dotfiles directory not found. Symlinking not successful."

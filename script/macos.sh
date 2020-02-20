a352154b8afabbbbb7ef3561550ec0d1f66735d4
Brendon Smith <br3ndonland@protonmail.com>
2020-02-20 14:47:26 -0500

    Add macOS TextEdit settings

--- script/macos.sh
+++ script/macos.sh
@@ -240,3 +240,13 @@ defaults write com.apple.mail-shared DisableURLLoading -bool true
 # Disable siri
 sudo launchctl unload -w /System/Library/LaunchAgents/com.apple.Siri.agent.plist
 
+###############################################################################
+# TextEdit                                                                    #
+###############################################################################
+
+# Use plain text mode for new TextEdit documents
+defaults write com.apple.TextEdit RichText -int 0
+
+# Open and save files as UTF-8 in TextEdit
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4

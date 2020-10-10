28f19a6eac9a762dffda172c3638a6fdc6257d12
Brendon Smith <br3ndonland@protonmail.com>
2020-10-10 19:35:18 -0400

    Add OS-specific VSCodium extension installation
    br3ndonland/dotfiles@ce224ae
    
    - Improve formatting of printed comments
    - Add `code`/`codium` command to PATH for macOS
    - Add VSCodium as default distro: `CODE_DISTRO=${CODE_DISTRO:-"codium"}`
    - Add error message if using Codespaces: Codespaces seems to have a
      limited version of the VSCode CLI that can't install extensions.
    
    https://code.visualstudio.com/docs/setup/mac
    https://github.com/microsoft/vscode/issues/42051
    https://docs.github.com/en/free-pro-team@latest/github/developing-online-with-codespaces

--- script/codium_extensions.sh
+++ script/codium_extensions.sh
@@ -8,7 +8,7 @@ install_codium_extensions() {
     printf "Error: %s command not in PATH.\n" "$EDITOR" >&2
     return 1
   fi
-  printf "Installing extensions for %s\n" "$EDITOR"
+  printf "\nInstalling extensions for %s\n\n" "$EDITOR"
   if [ "$EDITOR" = "code" ] || [ "$EDITOR" = "code-insiders" ]; then
     cat ~/.dotfiles/codium/extensions/marketplace-open-vsx.txt \
       ~/.dotfiles/codium/extensions/marketplace-proprietary.txt \
@@ -27,14 +27,15 @@ install_codium_extensions() {
     fi
   done <$EXTENSIONS
 }
+
 if [ -z "$1" ]; then
-  printf "Error: No editor was given for the codium_extensions.sh script.\n"
-  printf "Please specify ≥1 editor [code, code-insiders, codium].\n"
+  printf "\nError: No editor was given for the codium_extensions.sh script.\n"
+  printf "Please specify ≥1 editor: code code-insiders codium\n"
   exit 1
 fi
+
 for i in "$@"; do
   EDITOR=$i
-  # Add editor command to PATH
   case $i in
   code)
     APP="Visual\ Studio\ Code"
@@ -46,15 +47,12 @@ for i in "$@"; do
     APP="VSCodium"
     ;;
   esac
-  # TODO: Figure out how to use CLI without restarting terminal to install
-  # extensions via Strap. Currently, a terminal restart is needed before the CLI
-  # can be used. https://code.visualstudio.com/docs/setup/mac
-  # export PATH="$PATH:/Applications/$APP.app/Contents/Resources/app/bin"
-  printf "%s\n" $APP
-  # Install extensions
+  if [ "$(uname -s)" = "Darwin" ]; then
+    export PATH="/Applications/$APP.app/Contents/Resources/app/bin:$PATH"
+  fi
   if install_codium_extensions; then
-    printf "install_codium_extensions() successful for %s.\n" "$i"
+    printf "\nExtensions successfully installed for %s.\n" "$i"
   else
-    printf "Error: install_codium_extensions() unsuccessful for %s.\n" "$i"
+    printf "\nError: extensions not successfully installed for %s.\n" "$i"
   fi
 done

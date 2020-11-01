e2980f116bd8a5db17d17634843f5175fd50760a
Brendon Smith <br3ndonland@protonmail.com>
2020-11-01 06:20:21 -0500

    Generalize functions in symlink.sh
    - Abstract symlink code into separate functions
    - Symlink contents of dot directories (.config .gnupg .ssh), instead of
      the directories themselves, in case directories contain other files
    - Remove function for setting GPG perms (was only used once).

--- script/symlink.sh
+++ script/symlink.sh
@@ -2,48 +2,51 @@
 ### ---------------- Symlink dotfiles into home directory ----------------- ###
 # Run by strap-after-setup
 
-symlink_dotfiles() {
+symlink_dir_contents() {
+  TARGET_DIR=$3/${1##$2/}
+  ! [ -d "$TARGET_DIR" ] && mkdir -p "$TARGET_DIR"
+  for FILE in "$1/"*; do
+    symlink_file "$FILE" "$2" "$3"
+  done
+}
+
+symlink_file() {
+  ln -fns "$1" "$3/${1##$2/}"
+}
+
+symlink_repo_dotfiles() {
   echo "-> Symlinking dotfiles into home directory."
-  DOTS=$HOME/.dotfiles
-  NOTS=("$DOTS/.git" "$DOTS/.github" "$DOTS/.gitignore")
-  for FILE in "$DOTS/."*; do
-    ! [[ ${NOTS[*]} =~ $FILE ]] && ln -fns "$FILE" "$HOME/${FILE##$DOTS/}"
+  DOT_DIR=$HOME/.dotfiles
+  IGNORES=("$DOT_DIR/.git" "$DOT_DIR/.github" "$DOT_DIR/.gitignore")
+  for DOTFILE in "$DOT_DIR/."*; do
+    if ! [[ ${IGNORES[*]} =~ $DOTFILE ]]; then
+      [ -d "$DOTFILE" ] && symlink_dir_contents "$DOTFILE" "$DOT_DIR" "$HOME"
+      [ -f "$DOTFILE" ] && symlink_file "$DOTFILE" "$DOT_DIR" "$HOME"
+    fi
   done
 }
 
-symlink_vscodium() {
+symlink_vscodium_settings() {
   echo "-> Symlinking VSCodium settings."
-  DOTS=$HOME/.dotfiles/codium/User
+  SETTINGS_DIR=$HOME/.dotfiles/codium
   APP_DIR=$HOME/Library/Application\ Support
-  for DIR in $APP_DIR/{VSCodium,Code\ -\ Insiders}/User; do
-    ! [ -d "$DIR" ] && mkdir -p "$DIR"
-    for FILE in "$DOTS/"*; do
-      ln -fns "$FILE" "$DIR/${FILE##$DOTS/}"
-    done
+  for DIR in $APP_DIR/{VSCodium,Code\ -\ Insiders}; do
+    symlink_dir_contents "$SETTINGS_DIR/User" "$SETTINGS_DIR" "$DIR"
   done
 }
 
-set_perms() {
-  echo "-> Setting permissions on GPG directories."
-  chmod 700 ~/.gnupg
-  chmod 600 ~/.gnupg/gpg.conf
-}
-
-if symlink_dotfiles && symlink_vscodium; then
-  echo "-> Symlinking successful."
+if symlink_repo_dotfiles && symlink_vscodium_settings; then
+  echo "-> Symlinking successful. Finishing up..."
+  chmod 700 "$HOME"/.gnupg
+  chmod 600 "$HOME"/.gnupg/gpg.conf
   # https://pqrs.org/osx/karabiner/document.html#configuration-file-path
   # Restart Karabiner after symlinking config
   KARABINER=gui/"$(id -u)"/org.pqrs.karabiner.karabiner_console_user_server
   if (launchctl kickstart "$KARABINER"); then
     launchctl kickstart -k "$KARABINER"
   fi
+  echo "-> Finished."
 else
   echo "-> Symlinking unsuccessful."
-  ! [ -d ~/.dotfiles ] && echo "-> Error: Dotfiles directory not found"
-fi
-
-if set_perms; then
-  echo "-> Permissions successfully set on GPG directories."
-else
-  echo "-> Permissions not successfully set on GPG directories."
+  ! [ -d "$HOME"/.dotfiles ] && echo "-> Error: Dotfiles directory not found."
 fi

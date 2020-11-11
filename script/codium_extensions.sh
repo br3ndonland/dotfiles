49193b32868260df32988a6325b06842761dcb36
Brendon Smith <br3ndonland@protonmail.com>
2020-11-11 16:48:27 -0500

    Check VSCodium extension versions on Open VSX
    VSCodium/vscodium#483
    VSCodium/vscodium#494
    https://github.com/eclipse/openvsx/wiki/Registry-API
    
    The VSCodium update associated with VSCodium/vscodium#483 required
    reinstallation of extensions from the Open VSX registry, but there was
    no notification provided to prompt reinstallation, so extensions had
    gotten out-of-date.
    
    Open VSX has a nice API with Swagger/OpenAPI docs, and the API can be
    queried to check extension metadata. This commit will update the
    VSCode/VSCodium extension install script to check Open VSX and ensure
    the latest extension versions are installed.
    
    Note that it doesn't appear to be currently possible to install specific
    extension versions with the VSCodium CLI and Open VSX. Attempting to run
    `code --install-extension "$EXTENSION@$VERSION"` for a given extension
    and version returns a `500` error.

--- script/codium_extensions.sh
+++ script/codium_extensions.sh
@@ -1,14 +1,28 @@
-#!/bin/sh
+#!/usr/bin/env bash
 ### --------------------- Install VSCodium extensions --------------------- ###
 # Run by strap-after-setup
 # CLI: https://code.visualstudio.com/docs/editor/extension-gallery
 
-install_codium_extensions() {
-  if ! command -v "$EDITOR" >/dev/null; then
-    printf "Error: %s command not in PATH.\n" "$EDITOR" >&2
-    return 1
+check_open_vsx() {
+  if ! (command -v curl >/dev/null && command -v jq >/dev/null); then
+    printf "curl and jq required to check extension version.\n"
+    return
   fi
-  printf "\nInstalling extensions for %s\n\n" "$EDITOR"
+  URL="https://open-vsx.org/api/$(printf %s "$1" | cut -d @ -f 1 | tr '.' '/')"
+  LOCAL_VERSION=$(printf %s "$1" | cut -d @ -f 2)
+  OPEN_VSX_VERSION=$(
+    curl -fs -X GET "$URL" -H "accept: application/json" |
+      jq --raw-output .version
+  )
+  if [[ "$LOCAL_VERSION" == "$OPEN_VSX_VERSION" ]]; then
+    printf "Extension '%s' installed and up-to-date with Open VSX.\n" "$1"
+  else
+    $EDITOR --install-extension "$(printf %s "$1" | cut -d @ -f 1)"
+  fi
+}
+
+install_codium_extensions() {
+  printf "\nInstalling extensions for %s...\n\n" "$EDITOR"
   if [ "$EDITOR" = "code-insiders" ]; then
     cat ~/.dotfiles/codium/extensions/marketplace-open-vsx.txt \
       ~/.dotfiles/codium/extensions/marketplace-proprietary.txt \
@@ -17,14 +31,11 @@ install_codium_extensions() {
   else
     EXTENSIONS=~/.dotfiles/codium/extensions/marketplace-open-vsx.txt
   fi
-  while read -r EXTENSION; do
-    INSTALLED=$($EDITOR --list-extensions | grep -ce "^$EXTENSION\$")
-    if [ "$INSTALLED" = "0" ]; then
-      printf "Installing %s.\n" "$EXTENSION"
-      $EDITOR --install-extension "$EXTENSION"
-    else
-      printf "%s already installed.\n" "$EXTENSION"
-    fi
+  INSTALLED=("$($EDITOR --list-extensions --show-versions)")
+  while read -r EXT; do
+    EXT_INFO=$(printf %s "${INSTALLED[@]}" | grep "$EXT")
+    ! [ "$EXT_INFO" ] && $EDITOR --install-extension "$EXT" && continue
+    ! [ "$EDITOR" = "code-insiders" ] && check_open_vsx "$EXT_INFO" && continue
   done <$EXTENSIONS
 }
 
@@ -46,6 +57,10 @@ for i in "$@"; do
   esac
   if [ "$(uname -s)" = "Darwin" ]; then
     export PATH="/Applications/$APP.app/Contents/Resources/app/bin:$PATH"
+  fi
+  if ! command -v "$EDITOR" >/dev/null; then
+    printf "Error: %s command not in PATH.\n" "$EDITOR" >&2
+    return 1
   fi
   if install_codium_extensions; then
     printf "\nExtensions successfully installed for %s.\n" "$i"

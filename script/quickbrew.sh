c8faa6a1cabe78dfd6bc08a128d5ceaee1b81f75
Brendon Smith <br3ndonland@protonmail.com>
2020-11-23 12:04:27 +0000

    Improve quickbrew.sh
    - Use local Brewfile if present
    - Use installed Homebrew if present
    - Avoid exiting with errors
    - Add apt-get packages required by Homebrew and Docker

--- script/quickbrew.sh
+++ script/quickbrew.sh
@@ -3,30 +3,41 @@
 # A simple Linux setup script for Homebrew, Brew Bundle, and apt-get.
 
 # Download and install Homebrew: https://docs.brew.sh/Homebrew-on-Linux
-RAW="https://raw.githubusercontent.com"
-BREW_SCRIPT="Homebrew/install/master/install.sh"
-printf "\nDownloading and installing Homebrew.\n"
-printf "\n" | /usr/bin/env bash -c "$(curl -fsSL $RAW/$BREW_SCRIPT)"
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew &&
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-test -r ~/.bash_profile &&
-  echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-test -r ~/.zshrc &&
-  echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.zshrc
+if command -v brew >/dev/null 2>&1; then
+  echo "Homebrew detected."
+else
+  RAW="https://raw.githubusercontent.com"
+  BREW_SCRIPT="Homebrew/install/master/install.sh"
+  printf "\nDownloading and installing Homebrew.\n"
+  printf "\n" | /usr/bin/env bash -c "$(curl -fsSL $RAW/$BREW_SCRIPT)"
+  if [ -d /home/linuxbrew/.linuxbrew ]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  elif [ -d ~/.linuxbrew ]; then
+    eval "$(~/.linuxbrew/bin/brew shellenv)"
+  else
+    printf "Homebrew Linux directory not found."
+  fi
+fi
 
 # Install Homebrew dependencies from Brewfile with Brew Bundle
-printf "\nDownloading Brewfile and installing with Brew Bundle.\n"
-BREWFILE="${GITHUB_USER:-br3ndonland}/homebrew-brewfile/main/Brewfile"
-curl -fsSL "$RAW/$BREWFILE" | brew bundle check --file=- || brew bundle --global
+if [ -f ~/.Brewfile ]; then
+  brew bundle check --global
+else
+  printf "\nDownloading Brewfile and installing with Brew Bundle.\n"
+  BREWFILE="${GITHUB_USER:-br3ndonland}/homebrew-brewfile/main/Brewfile"
+  curl -fsSL "$RAW/$BREWFILE" | brew bundle --file=-
+fi
 
 # Install apt-get packages
-if [ "$1" ]; then
-  declare -a PACKAGES="[@]"
-  for PACKAGE in "${PACKAGES[@]}"; do
-    "sudo apt-get install -y $PACKAGE"
-  done
-else
-  printf "\nNo apt-get packages specified. No worries.\n"
-fi
+declare -a PACKAGES=(
+  apt-transport-https
+  build-essential
+  ca-certificates
+  curl
+  gnupg-agent
+  software-properties-common
+)
+[ "$1" ] && PACKAGES+=("[@]")
+for PACKAGE in "${PACKAGES[@]}"; do
+  sudo apt-get install -qy "$PACKAGE"
+done

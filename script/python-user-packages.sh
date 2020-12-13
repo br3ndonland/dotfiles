b133fb84e85cff7989ffd657248618a8edafdf1e
Brendon Smith <br3ndonland@protonmail.com>
2020-12-13 15:14:58 -0500

    Install from requirements.txt with pipx
    pipx is a helpful tool for managing user Python packages. One limitation
    is that pipx doesn't support package installation from requirements.txt.
    There's been some discussion of this in pipxproject/pipx#359.
    
    This commit will add a simple Bash script that reads requirements.txt
    and installs the packages therein with pipx. If the packages are already
    on `$PATH` (such as from installation with Homebrew), they are skipped.
    If the packages were installed with pipx on an older version of Python,
    they will be reinstalled. I sometimes need to run `pipx reinstall-all`
    after updating to a new minor version of Python, and this script will
    approximate the behavior of `pipx reinstall-all`.
    
    At the moment, this script doesn't support all requirements.txt syntax,
    such as version specification. I don't normally need to pin versions of
    pipx packages, so this is fine for me.

---
+++ script/python-user-packages.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+### --------------------- Install user Python packages --------------------- ###
+# Run by strap-after-setup
+# Manage user Python packages after install: https://github.com/pipxproject/pipx
+
+pipx_install_requirements() {
+  printf "\n-> Installing user Python packages with pipx...\n\n"
+  PY="$(python3 --version)"
+  PIPX_LIST="$(pipx list)"
+  while read -r P; do
+    [[ $P =~ "httpie" ]] && CMD="http" || CMD="$P"
+    if [[ $(echo "$PIPX_LIST" | grep -ce "package $P .*, $PY") -gt 0 ]]; then
+      echo "$P already installed."
+    elif [[ $(echo "$PIPX_LIST" | grep -ce "package $P") -gt 0 ]]; then
+      echo "Reinstalling $P for $PY."
+      pipx uninstall "$P" && pipx install --python "$(command -v python3)" "$P"
+    elif command -v "$CMD" &>/dev/null; then
+      echo "$P already on PATH at $(command -v "$CMD")."
+    else
+      pipx install "$P"
+    fi
+  done <"$HOME/.dotfiles/py/requirements.txt"
+}
+
+if ! command -v pipx &>/dev/null; then
+  printf "\n-> Error: pipx must be installed and on PATH.\n\n"
+  exit 1
+elif pipx_install_requirements; then
+  printf "\n-> Successful pipx package installation.\n\n"
+else
+  printf "\n-> Unsuccessful pipx package installation.\n\n"
+fi

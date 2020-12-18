br3ndonland/dotfiles

commit 40e0ddaa7e0781d4b57cbe277465a154f8313061

Switch VSCode extension script from jq to fx
https://github.com/nodejs/node/releases/tag/v15.3.0
https://github.com/antonmedv/fx
https://github.com/stedolan/jq

jq hasn't had a release in two years, and I'm not yet able to build it
from source on Apple Silicon. fx is easier to use than jq, and depends
on Node.js, which can build from source on Apple Silicon as of v15.3.0.
trentm/json is also a good option.

script/vscode-extensions.sh
@@ -3,18 +3,18 @@
 # CLI: https://code.visualstudio.com/docs/editor/extension-gallery
 
 check_open_vsx() {
-  if ! (command -v curl >/dev/null 2>&1 && command -v jq >/dev/null 2>&1); then
-    printf "curl and jq required to check extension version.\n"
+  if ! command -v curl >/dev/null 2>&1 && ! command -v fx >/dev/null 2>&1; then
+    printf "curl and antonmedv/fx required to check extension version.\n"
     return
   fi
   URL="https://open-vsx.org/api/$(printf %s "$1" | cut -d @ -f 1 | tr '.' '/')"
   LOCAL_VERSION=$(printf %s "$1" | cut -d @ -f 2)
   OPEN_VSX_VERSION=$(
     curl -fs -X GET "$URL" -H "accept: application/json" |
-      jq --raw-output .version
+      fx .version
   )
   if [[ "$LOCAL_VERSION" == "$OPEN_VSX_VERSION" ]]; then
-    printf "Extension '%s' installed and up-to-date with Open VSX.\n" "$1"
+    printf "Extension '%s' up-to-date with Open VSX.\n" "$1"
   else
     $EDITOR --install-extension "$(printf %s "$1" | cut -d @ -f 1)"
   fi

commit 1b7cf02d6b69ca7d67b10dea1bf284db9a9a1d8b

Correct command check in vscode-extensions.sh
Commit 40e0dda introduced a minor syntax error when checking for CLIs.
This commit will fix the syntax error, restoring the previous syntax.

script/vscode-extensions.sh
@@ -3,7 +3,7 @@
 # CLI: https://code.visualstudio.com/docs/editor/extension-gallery
 
 check_open_vsx() {
-  if ! command -v curl &>/dev/null && ! command -v fx &>/dev/null; then
+  if ! (command -v curl &>/dev/null && command -v fx &>/dev/null); then
     printf "curl and antonmedv/fx required to check extension version.\n"
     return
   fi
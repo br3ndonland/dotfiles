2eba3a8f7fb367baabcc7ee11343e4ea968dfbf6
Brendon Smith <br3ndonland@protonmail.com>
2021-01-15 22:46:07 -0500

    Enable Homebrew on Apple Silicon
    br3ndonland/dotfiles@9f59c95
    br3ndonland/dotfiles@f7b418d
    https://brew.sh/2021/02/05/homebrew-3.0.0/
    
    Homebrew is now compatible with Apple Silicon. This commit will update
    bootstrap.sh to install Homebrew on Apple Silicon Macs, either using the
    install script from https://github.com/Homebrew/install, or by setting
    up the Homebrew prefix and cloning the Homebrew repo manually.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ### ------------ bootstrap.sh - set up development environment ------------ ###
-# https://github.com/MikeMcQuaid/strap, https://github.com/MikeMcQuaid/dotfiles
+# https://github.com/MikeMcQuaid/strap, https://github.com/Homebrew/install
 
 set -e
 
@@ -8,6 +8,12 @@ case $(uname -s) in
 Darwin)
   export MACOS=1 && export UNIX=1
   CPU=$(sysctl -n machdep.cpu.brand_string)
+  UNAME_MACHINE=$(uname -m)
+  if [[ $CPU == *Apple* ]] || [[ $UNAME_MACHINE == "arm64" ]]; then
+    DEFAULT_HOMEBREW_PREFIX="/opt/homebrew"
+  elif [[ $CPU == *Intel* ]] || [[ $UNAME_MACHINE == "x86_64" ]]; then
+    DEFAULT_HOMEBREW_PREFIX="/usr/local"
+  fi
   ;;
 Linux)
   export LINUX=1 && export UNIX=1
@@ -17,7 +23,12 @@ codespace)
   ;;
 esac
 
+STDIN_FILE_DESCRIPTOR="0"
 [[ "$1" = "--debug" || -o xtrace ]] && STRAP_DEBUG="1"
+[ -t "$STDIN_FILE_DESCRIPTOR" ] && STRAP_INTERACTIVE="1"
+STRAP_GIT_NAME=${STRAP_GIT_NAME:-"Brendon Smith"}
+STRAP_GIT_EMAIL=${STRAP_GIT_EMAIL:-"br3ndonland@protonmail.com"}
+STRAP_GITHUB_USER=${STRAP_GITHUB_USER:-"br3ndonland"}
 STRAP_SUCCESS=""
 
 sudo_askpass() {
@@ -51,12 +62,6 @@ else
   STRAP_QUIET_FLAG="-q"
   Q="$STRAP_QUIET_FLAG"
 fi
-
-STDIN_FILE_DESCRIPTOR="0"
-[ -t "$STDIN_FILE_DESCRIPTOR" ] && STRAP_INTERACTIVE="1"
-STRAP_GIT_NAME=${STRAP_GIT_NAME:-"Brendon Smith"}
-STRAP_GIT_EMAIL=${STRAP_GIT_EMAIL:-"br3ndonland@protonmail.com"}
-STRAP_GITHUB_USER=${STRAP_GITHUB_USER:-"br3ndonland"}
 
 # Prompt for sudo password and initialize (or reinitialize) sudo
 sudo --reset-timestamp
@@ -156,9 +161,9 @@ run_dotfile_scripts() {
   fi
 }
 
-[ "$USER" = "root" ] && abort "Run Strap as yourself, not root."
+[ "$USER" = "root" ] && abort "Run bootstrap.sh as yourself, not root."
 
-if [ $MACOS ]; then
+if [ $MACOS ] && [ $MACOS -gt 0 ]; then
   [ -z "$STRAP_CI" ] && caffeinate -s -w $$ &
   groups | grep $Q -E "\b(admin)\b" || abort "Add $USER to admin."
   logn "Configuring security settings:"
@@ -174,14 +179,14 @@ if [ $MACOS ]; then
     /Library/Preferences/com.apple.alf globalstate -int 1
   sudo_askpass launchctl load \
     /System/Library/LaunchDaemons/com.apple.alf.agent.plist 2>/dev/null
-fi
-if [ $MACOS ] && [ -n "$STRAP_GIT_NAME" ] && [ -n "$STRAP_GIT_EMAIL" ]; then
-  FOUND="Found this computer? Please contact"
-  LOGIN_TEXT=$(escape "$FOUND $STRAP_GIT_NAME at $STRAP_GIT_EMAIL.")
-  echo "$LOGIN_TEXT" | grep -q '[()]' && LOGIN_TEXT="'$LOGIN_TEXT'"
-  sudo_askpass defaults write \
-    /Library/Preferences/com.apple.loginwindow LoginwindowText "$LOGIN_TEXT"
-  logk
+  if [ -n "$STRAP_GIT_NAME" ] && [ -n "$STRAP_GIT_EMAIL" ]; then
+    FOUND="Found this computer? Please contact"
+    LOGIN_TEXT=$(escape "$FOUND $STRAP_GIT_NAME at $STRAP_GIT_EMAIL.")
+    echo "$LOGIN_TEXT" | grep -q '[()]' && LOGIN_TEXT="'$LOGIN_TEXT'"
+    sudo_askpass defaults write \
+      /Library/Preferences/com.apple.loginwindow LoginwindowText "$LOGIN_TEXT"
+    logk
+  fi
 fi
 
 # Check and enable full-disk encryption.
@@ -204,7 +209,7 @@ else
 fi
 
 # Set up Xcode Command Line Tools.
-install_xcode() {
+install_xcode_clt() {
   if ! [ -f "/Library/Developer/CommandLineTools/usr/bin/git" ]; then
     log "Installing the Xcode Command Line Tools:"
     CLT_STRING=".com.apple.dt.CommandLineTools.installondemand.in-progress"
@@ -244,8 +249,8 @@ check_xcode_license() {
   fi
 }
 
-if [ $MACOS ]; then
-  install_xcode
+if [ $MACOS ] && [ $MACOS -gt 0 ]; then
+  install_xcode_clt
   check_xcode_license
 else
   echo "Xcode skipped."
@@ -275,8 +280,7 @@ if git credential-osxkeychain 2>&1 | grep $Q "git.credential-osxkeychain"; then
   if [ "$(git config --global credential.helper)" != "osxkeychain" ]; then
     git config --global credential.helper osxkeychain
   fi
-
-  if [ -n "$STRAP_GITHUB_USER" ]; then
+  if [ -n "$STRAP_GITHUB_USER" ] && [ -n "$STRAP_GITHUB_TOKEN" ]; then
     PROTOCOL="protocol=https\\nhost=github.com"
     printf "%s\\n" "$PROTOCOL" | git credential-osxkeychain erase
     printf "%s\\nusername=%s\\npassword=%s\\n" \
@@ -293,7 +297,7 @@ logn "Checking for software updates:"
 if softwareupdate -l 2>&1 | grep $Q "No new software available."; then
   logk
 else
-  if [ $MACOS ] && [ -z "$STRAP_CI" ]; then
+  if [ $MACOS ] && [ $MACOS -gt 0 ] && [ -z "$STRAP_CI" ]; then
     echo
     log "Installing software updates:"
     sudo_askpass softwareupdate --install --all
@@ -350,11 +354,9 @@ fi
 install_homebrew() {
   logn "Installing Homebrew:"
   HOMEBREW_PREFIX="$(brew --prefix 2>/dev/null || true)"
-  [ -n "$HOMEBREW_PREFIX" ] || HOMEBREW_PREFIX="/usr/local"
+  [ -n "$HOMEBREW_PREFIX" ] || HOMEBREW_PREFIX="$DEFAULT_HOMEBREW_PREFIX"
   [ -d "$HOMEBREW_PREFIX" ] || sudo_askpass mkdir -p "$HOMEBREW_PREFIX"
-  if [ "$HOMEBREW_PREFIX" = "/usr/local" ]; then
-    sudo_askpass chown "root:wheel" "$HOMEBREW_PREFIX" 2>/dev/null || true
-  fi
+  sudo_askpass chown "root:wheel" "$HOMEBREW_PREFIX" 2>/dev/null || true
   (
     cd "$HOMEBREW_PREFIX"
     sudo_askpass mkdir -p Cellar Frameworks bin etc include lib opt sbin share var
@@ -362,13 +364,12 @@ install_homebrew() {
       Cellar Frameworks bin etc include lib opt sbin share var
   )
   HOMEBREW_REPOSITORY="$(brew --repository 2>/dev/null || true)"
-  [ -n "$HOMEBREW_REPOSITORY" ] || HOMEBREW_REPOSITORY="/usr/local/Homebrew"
+  [ -n "$HOMEBREW_REPOSITORY" ] || HOMEBREW_REPOSITORY="$HOMEBREW_PREFIX/Homebrew"
   [ -d "$HOMEBREW_REPOSITORY" ] || sudo_askpass mkdir -p "$HOMEBREW_REPOSITORY"
   sudo_askpass chown -R "$USER:admin" "$HOMEBREW_REPOSITORY"
   if [ $HOMEBREW_PREFIX != $HOMEBREW_REPOSITORY ]; then
     ln -sf "$HOMEBREW_REPOSITORY/bin/brew" "$HOMEBREW_PREFIX/bin/brew"
   fi
-  # Download Homebrew.
   export GIT_DIR="$HOMEBREW_REPOSITORY/.git" GIT_WORK_TREE="$HOMEBREW_REPOSITORY"
   git init $Q
   git config remote.origin.url "https://github.com/Homebrew/brew"
@@ -377,7 +378,6 @@ install_homebrew() {
   git reset $Q --hard origin/master
   unset GIT_DIR GIT_WORK_TREE
   logk
-  # Update Homebrew.
   export PATH="$HOMEBREW_PREFIX/bin:$PATH"
   log "Updating Homebrew:"
   brew update
@@ -408,13 +408,12 @@ run_brew_installs() {
   fi
 }
 
-if [[ $MACOS ]] && [[ "$CPU" =~ "Intel" ]]; then
-  install_homebrew
+if [ $MACOS ] && [ $MACOS -gt 0 ]; then
+  RAW="https://raw.githubusercontent.com"
+  BREW_SCRIPT="Homebrew/install/HEAD/install.sh"
+  /usr/bin/env bash -c "$(curl -fsSL $RAW/$BREW_SCRIPT)" || install_homebrew
   run_brew_installs
-elif [[ "$CPU" =~ "Apple" ]]; then
-  echo "Skipping Homebrew installs for Mac with non-Intel CPU: $CPU."
-  echo "Homebrew prefix for Apple Silicon processors will be /opt/homebrew"
-elif $LINUX; then
+elif [ $LINUX ] && [ $LINUX -gt 0 ]; then
   run_dotfile_scripts script/linuxbrew.sh
 else
   echo "Skipping Homebrew installs."

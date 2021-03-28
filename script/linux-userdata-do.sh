6650876e6393cabc370e35302e741d46b8a0da2c
Brendon Smith <br3ndonland@protonmail.com>
2021-03-28 00:20:09 -0400

    Simplify syntax in Linux userdata script
    - Use environment variable instead of script argument to set username:
      userdata script inputs don't usually allow arguments to be passed in
    - Simplify `sed` syntax: use `-i` abbreviation instead of `--in-place`,
      and use pipes(`|`) with extended regex syntax

--- script/linux-userdata-do.sh
+++ script/linux-userdata-do.sh
@@ -3,9 +3,9 @@
 # https://www.digitalocean.com/docs/droplets/tutorials/recommended-setup/
 # https://www.digitalocean.com/docs/recommended-droplet-setup.sh
 
-set -euo pipefail
+set -eou pipefail
 
-USERNAME=${1:-"brendon"} # Customize the sudo non-root username here
+USERNAME=${USERNAME:="brendon"} # Customize the sudo non-root username here
 
 # Create user and immediately expire password to force a change on login
 useradd --create-home --shell "/bin/bash" --groups sudo "${USERNAME}"
@@ -13,7 +13,7 @@ passwd --delete "${USERNAME}"
 chage --lastday 0 "${USERNAME}"
 
 # Create SSH directory for sudo user and move keys over
-HOME_DIR="$(eval echo ~"${USERNAME}")"
+HOME_DIR="$(eval echo ~${USERNAME})"
 mkdir --parents "${HOME_DIR}/.ssh"
 cp /root/.ssh/authorized_keys "${HOME_DIR}/.ssh"
 chmod 0700 "${HOME_DIR}/.ssh"
@@ -21,7 +21,6 @@ chmod 0600 "${HOME_DIR}/.ssh/authorized_keys"
 chown --recursive "${USERNAME}":"${USERNAME}" "${HOME_DIR}/.ssh"
 
 # Disable root SSH login with password
-sed --in-place \
-  's/^PermitRootLogin.*/PermitRootLogin prohibit-password/g' \
+sed -i -E 's|^PermitRootLogin.*|PermitRootLogin prohibit-password|g' \
   /etc/ssh/sshd_config
 if sshd -t -q; then systemctl restart sshd; fi

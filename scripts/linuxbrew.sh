ddc8d3a2acc239563970060881f1b7901314522f
Brendon Smith <bws@bws.bio>
2022-09-17 14:06:50 -0400

    Add Proton VPN CLI install to Linux setup script
    https://protonvpn.com/support/linux-vpn-setup/

--- scripts/linuxbrew.sh
+++ scripts/linuxbrew.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 
 # Download and install Homebrew: https://docs.brew.sh/Homebrew-on-Linux
 if command -v brew &>/dev/null; then
-  printf "Homebrew detected."
+  printf "\nHomebrew detected.\n"
 else
   RAW="https://raw.githubusercontent.com"
   BREW_SCRIPT="Homebrew/install/HEAD/install.sh"
@@ -24,12 +24,22 @@ fi
 skips="awscli black deno jupyterlab macos-trash mas zsh-completions"
 export HOMEBREW_BUNDLE_BREW_SKIP="$skips"
 if [ -f "$HOME/.Brewfile" ]; then
+  printf "\nInstalling %s with Brew Bundle.\n" "$HOME/.Brewfile"
   brew bundle check --global || brew bundle --global
 else
   printf "\nDownloading Brewfile and installing with Brew Bundle.\n"
   BREWFILE="${STRAP_GITHUB_USER:-br3ndonland}/dotfiles/HEAD/Brewfile"
   curl -fsSL "$RAW/$BREWFILE" | brew bundle --file=-
 fi
+
+# Set up Proton VPN
+deb_file="protonvpn-stable-release_1.0.1-1_all.deb"
+deb_path="$HOME/Downloads/$deb_file"
+printf "\nDownloading Proton VPN setup DEB to %s\n" "$deb_path"
+curl -fsSL -o "$deb_path" "https://protonvpn.com/download/$deb_file"
+printf "\nInstalling Proton VPN setup DEB from %s\n" "$deb_path"
+sudo apt-get install -qy "$deb_path"
+sudo apt-get update
 
 # Install apt-get packages
 PACKAGES=(
@@ -38,6 +48,7 @@ PACKAGES=(
   ca-certificates
   curl
   gnupg-agent
+  protonvpn-cli
   software-properties-common
 )
 [ -n "$1" ] && PACKAGES+=("[@]")

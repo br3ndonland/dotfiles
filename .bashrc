br3ndonland/dotfiles

commit 909c1e7a4dd5986d8bece38eb62c663956119dc4

Add .zfunc for for Zsh shell functions

.zshrc
@@ -48,5 +48,5 @@ bindkey '^N' kill-buffer          # ctrl+n     delete all lines
 bindkey '^_' undo                 # ctrl+/     undo
 bindkey '^\' redo                 # ctrl+\     redo
 ### ---------------------------- Integrations ----------------------------- ###
-fpath=(/usr/local/share/zsh-completions $fpath)
+fpath+=~/.zfunc:/usr/local/share/zsh-completions
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh

script/strap-after-setup
@@ -20,6 +20,9 @@ curl -sSL \
   https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py |
   python
 
+# Create directory for Zsh shell functions
+mkdir ~/.zfunc
+
 # Set shell
 echo "-> Changing shell to Zsh. Password entry required, enabled with --debug."
 if [ "$SHELL" != "/bin/zsh" ]; then

commit bf35c4aef43be4f4a981cbc622f8bdd52acff1b9

Add IP checker function to `$fpath`
It is sometimes helpful to check a machine's public IP address from the
command-line. The free ip.me service from Proton is one way to do this.
Running `curl ip.me` will return just the IP address in plain-text, but
other HTTP clients like wget or HTTPie will return the full webpage.
This is due to how ip.me handles the user agent. Although it is not
documented on the curl manpage, curl sets `User-Agent:curl/[version]`,
such as `User-Agent:curl/8.11.0` (the current version). Similar
behavior can therefore be achieved by setting the user agent like curl.

These commands all produce equivalent results:

```sh
curl ip.me
wget -qO - -U curl/8.11.0 ip.me
http ip.me User-Agent:curl/8.11.0 -b # https://httpie.io/
```

This commit will add a script for checking the IP address into a new
directory `.zfunc`. This directory is set up so the function can be
loaded automatically by shells like Bash and Zsh. For Bash, the
directory will simply be added to `$PATH`. For Zsh, the directory will
be added to `$fpath` (`fpath+=($HOME/.zfunc)`), a special array variable
telling Zsh where to look for shell functions, and then automatically
loaded into each shell (`autoload -Uz $HOME/.zfunc/*(:tX)`). `(:tX)` is
a special Zsh expansion parameter that lists the filenames `(:t)` of all
executable files (`X`) in the given directory.

As this IP script may be used by multiple shells, the shebang will be
omitted and a directive will ShellCheck to treat it as POSIX sh.

As part of this addition to the `$fpath`, this commit will also fix a
long-standing error in the `$fpath` introduced in commit 909c1e7 that
reflected a misunderstanding of the `$fpath` and of the difference
between `$fpath` and `$FPATH`. The syntax introduced in 909c1e7 modified
`$fpath`, but with `$FPATH` syntax, which is similar to `$PATH` syntax
(a string with colon-separated values). However, `$fpath` is actually an
array. This commit will correct the syntax error.

.bashrc
@@ -4,6 +4,9 @@
 HISTCONTROL=ignoreboth
 shopt -s globstar histappend nullglob
 
+### exports
+export PATH=$HOME/.zfunc:$PATH
+
 ### aliases
 alias python='python3'
 

.zfunc/ip
@@ -1,0 +1,16 @@
+# shellcheck shell=sh
+
+curl_version="8.11.0"
+if type http >/dev/null 2>&1; then
+  http ip.me User-Agent:curl/$curl_version -b
+elif type curl >/dev/null 2>&1; then
+  curl ip.me
+elif type wget >/dev/null 2>&1; then
+  wget -qO - -U curl/$curl_version ip.me
+else
+  echo "Error in ip function." \
+    "Please install a command-line HTTP client" \
+    "and update the function to use that client."
+  return 1
+fi
+unset curl_version

.zshrc
@@ -90,9 +90,15 @@ source <(starship init zsh)
 ### mise: https://mise.jdx.dev/
 eval "$(mise activate zsh)"
 
+### functions
+# ensure .zfunc is symlinked to $HOME/.zfunc
+typeset -U fpath
+fpath+=($HOME/.zfunc)
+autoload -Uz $HOME/.zfunc/*(:tX)
+
 ### completions
 if type brew &>/dev/null && [[ -d $HOMEBREW_PREFIX ]]; then
-  fpath+=$HOME/.zfunc:$HOMEBREW_PREFIX/share/zsh/site-functions
+  fpath+=($HOMEBREW_PREFIX/share/zsh/site-functions)
 fi
 zstyle :compinstall filename $HOME/.zshrc
 autoload -Uz compinit
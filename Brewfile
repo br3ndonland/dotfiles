tamsky/br3ndonland-dotfiles

Adjust dotfiles for work

Brewfile
@@ -5,7 +5,7 @@ tap "homebrew/services"
 brew "awscli"
 brew "bash"
 brew "black", restart_service: :changed
-brew "ffmpeg"
+# brew "ffmpeg"
 brew "findutils"
 brew "flake8"
 brew "gh"
@@ -16,25 +16,25 @@ brew "gnu-sed"
 brew "gnu-tar"
 brew "go"
 brew "grep"
-# brew "helm"
+brew "helm"
 brew "httpie"
 brew "jq"
 brew "jupyterlab"
-brew "libfido2"
+# brew "libfido2"
 brew "macos-trash" if OS.mac?
-brew "mas" if OS.mac?
-brew "media-info"
-brew "mkvtoolnix"
-brew "mp4v2"
+# brew "mas" if OS.mac?
+# brew "media-info"
+# brew "mkvtoolnix"
+# brew "mp4v2"
 brew "mypy"
-# brew "mysql-client"
+brew "mysql-client"
 brew "node"
 brew "openssh"
 brew "opentofu"
-brew "pinentry"
+# brew "pinentry"
 # brew "pinentry-mac"
 brew "pipx"
-brew "pnpm"
+# brew "pnpm"
 brew "postgresql@15", restart_service: :changed
 brew "pre-commit"
 brew "prettier"
@@ -48,10 +48,10 @@ brew "starship"
 brew "taplo"
 brew "terraform@1.5"
 brew "terragrunt"
-# brew "terraformer"
+brew "terraformer"
 brew "typescript"
-brew "wrangler"
-brew "ykman"
+# brew "wrangler"
+# brew "ykman"
 brew "zsh"
 brew "zsh-completions"
 brew "zsh-syntax-highlighting"
@@ -59,31 +59,31 @@ brew "zsh-syntax-highlighting"
 cask "1password"
 cask "1password-cli"
 cask "brave-browser"
-# cask "datagrip"
+cask "datagrip"
 cask "docker"
 cask "firefox"
 # cask "inkscape"
 cask "karabiner-elements"
 cask "kitty"
-# cask "notion"
-# cask "postico"
+cask "notion"
+cask "postico"
 # cask "postman"
-cask "protonvpn"
+# cask "protonvpn"
 # cask "rstudio"
 # cask "session-manager-plugin"
-# cask "slack"
-cask "syncthing"
-# cask "visual-studio-code"
+cask "slack"
+# cask "syncthing"
+cask "visual-studio-code"
 cask "vlc"
 cask "vscodium"
-# cask "zoom"
+cask "zoom"
 cask "zotero" # no arm64_big_sur yet
 
 # mas "1password", id: 1333542190
-mas "bear", id: 1091189122
-mas "daisydisk", id: 411643860
-mas "duckduckgo privacy essentials", id: 1482920575
-mas "keynote", id: 409183694
-mas "numbers", id: 409203825
-mas "pages", id: 409201541
-mas "pdf expert", id: 1055273043
+# mas "bear", id: 1091189122
+# mas "daisydisk", id: 411643860
+# mas "duckduckgo privacy essentials", id: 1482920575
+# mas "keynote", id: 409183694
+# mas "numbers", id: 409203825
+# mas "pages", id: 409201541
+# mas "pdf expert", id: 1055273043

br3ndonland/dotfiles

Brewfile
@@ -15,14 +15,14 @@ brew "grep"
 brew "libfido2"
 brew "mas" if OS.mac?
 brew "mise"
-# brew "opencode"
+brew "opencode"
 brew "openssh"
 brew "pinentry"
 brew "postgresql@17"
-brew "r"
+# brew "r"
 brew "ripgrep"
 brew "starship"
-brew "syncthing"
+# brew "syncthing"
 brew "tmux"
 brew "tree"
 brew "vim"
@@ -32,12 +32,12 @@ brew "zsh-completions"
 brew "zsh-syntax-highlighting"
 brew "zsh"
 
-cask "1password"
+# cask "1password"
 cask "1password-cli"
 cask "brave-browser"
-# cask "chatgpt"
-# cask "codex"
-# cask "cursor"
+cask "chatgpt"
+cask "codex"
+cask "cursor"
 # cask "datagrip"
 # cask "docker"
 cask "font-recursive-code"
@@ -48,23 +48,23 @@ cask "kitty"
 # cask "microsoft-office-businesspro"
 # cask "notion"
 cask "orbstack"
-# cask "postico"
+cask "postico"
 # cask "postman"
-cask "protonvpn"
+# cask "protonvpn"
 # cask "rstudio"
-# cask "session-manager-plugin"
+cask "session-manager-plugin"
 # cask "slack"
 # cask "tableplus"
 # cask "visual-studio-code"
 cask "vlc"
 cask "vscodium"
-# cask "zoom"
+cask "zoom"
 cask "zotero"
 
 # mas "1password", id: 1333542190
-mas "1password for safari", id: 1569813296
-mas "bear", id: 1091189122
-mas "daisydisk", id: 411643860
-mas "keynote", id: 409183694
-mas "numbers", id: 409203825
-mas "pages", id: 409201541
+# mas "1password for safari", id: 1569813296
+# mas "bear", id: 1091189122
+# mas "daisydisk", id: 411643860
+# mas "keynote", id: 409183694
+# mas "numbers", id: 409203825
+# mas "pages", id: 409201541
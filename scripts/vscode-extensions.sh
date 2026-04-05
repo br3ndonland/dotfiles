fa15c0955291d56835ba09bc34c811dc0cf5dad7
Brendon Smith <bws@bws.bio>
2026-04-05 19:50:16 -0400

    Unify VSCode/VSCodium extension version checks
    The VSCode/VSCodium extension script `scripts/vscode-extensions.sh`
    supports installing the latest versions, like `publisher.extension`, or
    pinned versions, like `publisher.extension@version`. Pinned versions are
    not properly respected, and the strategy for updating versions differs
    between the Microsoft marketplace and the Open VSX marketplace. For the
    Open VSX marketplace, the script was originally written (49193b3) to
    check the Open VSX API and only reinstall extensions with `--force` if
    the version was outdated. The Microsoft marketplace did not have this
    support, so the script did not check Microsoft VSCode versions.
    
    This commit will update `scripts/vscode-extensions.sh` to respect pinned
    versions and to handle updates the same way for all marketplaces. For
    unpinned versions, the script will use the `--force` option every time
    so the VSCode/VSCodium CLI checks the marketplace version as part of the
    built-in behavior of `--install-extension`. There's also not really any
    need to check the list of installed extensions and add conditions based
    on that, because the VSCode CLI now just says the extension is already
    installed and exits with a `0` success exit code, so it will be removed.
    
    https://code.visualstudio.com/docs/configure/command-line#_working-with-extensions

--- scripts/vscode-extensions.sh
+++ scripts/vscode-extensions.sh
@@ -2,34 +2,9 @@
 ### ---------------------- Install VSCode extensions ---------------------- ###
 # CLI: https://code.visualstudio.com/docs/editor/extension-marketplace
 
-check_open_vsx() {
-  if ! type curl &>/dev/null || ! (
-    type fx || type jq
-  ) &>/dev/null; then
-    printf "curl and jq or antonmedv/fx required to check extension version.\n"
-    return
-  fi
-  local local_version open_vsx_version response url
-  extension_name=$(printf %s "$2" | cut -d @ -f 1)
-  local_version=$(printf %s "$2" | cut -d @ -f 2)
-  url="https://open-vsx.org/api/$(echo "$extension_name" | tr '.' '/')"
-  response=$(curl -fs "$url" -H "accept: application/json")
-  if type fx &>/dev/null; then
-    open_vsx_version=$(printf %s "$response" | fx .version)
-  elif type jq &>/dev/null; then
-    open_vsx_version=$(printf %s "$response" | jq -r .version)
-  fi
-  if [ "$local_version" = "$open_vsx_version" ]; then
-    printf "Extension '%s' up-to-date with Open VSX.\n" "$2"
-  else
-    printf "Updating to %s\n" "$extension_name@$open_vsx_version"
-    $1 --install-extension "$extension_name@$open_vsx_version" --force
-  fi
-}
-
 install_extensions() {
   printf "\nInstalling extensions for '%s'...\n\n" "$1"
-  local extension_info extension_name extensions installed
+  local extension_name extensions latest
   case $1 in
   code*)
     cat \
@@ -42,24 +17,15 @@ install_extensions() {
     extensions="$HOME/.dotfiles/vscode/extensions/marketplace-open-vsx.txt"
     ;;
   esac
-  installed=("$($1 --list-extensions --show-versions)")
   while read -r extension; do
     case $extension in
     '#'*) printf "Skipping commented extension '%s'\n" "$extension" && continue ;;
-    *) printf "Checking extension '%s'\n" "$extension" ;;
+    *) printf "Installing extension '%s'\n" "$extension" ;;
     esac
     extension_name=$(printf %s "$extension" | cut -d @ -f 1)
-    if [ "$extension" = "$extension_name" ]; then
-      extension_info=$(printf %s "${installed[@]}" | grep "$extension_name@")
-    else
-      extension_info=$(printf %s "${installed[@]}" | grep "$extension")
-    fi
-    if [ -n "$extension_info" ]; then
-      printf "Extension '%s' installed.\n" "$extension_info"
-      if [ "$1" = "codium" ]; then check_open_vsx "$1" "$extension_info"; fi
-    else
-      $1 --install-extension "$extension"
-    fi
+    [ "$extension" = "$extension_name" ] && latest=1 || true
+    $1 --install-extension "$extension" ${latest:+ --force}
+    printf '\n'
   done <"$extensions"
 }
 
@@ -86,8 +52,8 @@ for i in "$@"; do
     printf "\nError: %s command not on PATH.\n" "$i" >&2
     exit 1
   elif install_extensions "$i"; then
-    printf "\nExtensions successfully installed for %s.\n" "$i"
+    printf "Extensions successfully installed for %s.\n" "$i"
   else
-    printf "\nError: extensions not successfully installed for %s.\n" "$i"
+    printf "Error: extensions not successfully installed for %s.\n" "$i"
   fi
 done

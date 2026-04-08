26c595d55eac8f5872822352da9dfd1b7845c6f6
Brendon Smith <bws@bws.bio>
2026-04-07 23:45:13 -0400

    Simplify ShellCheck command
    ShellCheck has limited support for excluding or including files, so a
    workaround was used to input a list of files. This commit will simplify
    the workaround to leverage `git ls-files` instead. An obvious limitation
    is that the command now requires a Git repo, so for contexts not using
    Git repos, the previous script may be preferable.
    
    https://github.com/koalaman/shellcheck/issues/2011

--- scripts/shellcheck.sh
+++ scripts/shellcheck.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-shopt -s dotglob globstar nullglob
-
-set -- **/*.{bash,sh,zfunc/}*
-
-if [ -n "$1" ]; then
-  printf "Found the following files:\n"
-  printf "%s\n" "$@"
-  shellcheck "$@"
-else
-  echo "No shell scripts for ShellCheck to check."
-fi
+git ls-files '*.sh' '*.bash' | xargs --no-run-if-empty shellcheck
